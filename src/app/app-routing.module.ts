8f10490ecdd72386a06d61f87ce93f92e8938511
WalterGimenez <wallegimenez@gmail.com>
2022-03-12 14:50:15 -0300

    creación porfolio con header y redes incompleto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { PaginaErrorComponent } from './component/pagina-error/pagina-error.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: HeaderComponent, pathMatch: 'full'},
+  {path:'**', component: PaginaErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaErrorComponent } from './component/pagina-error/pagina-error.component';
+import { ApLogoComponent } from './component/ap-logo/ap-logo.component';
+import { RedesComponent } from './component/redes/redes.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaErrorComponent,
+    ApLogoComponent,
+    RedesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
